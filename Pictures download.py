5039b1124765ed7ac39e5acd9a62fc65897dc860
lxdxlxd <lxdxlxd@v1020-wn-116-237.campus-dynamic.uwaterloo.ca>
2016-09-25 20:02:51 -0400

    Initial

---
+++ Pictures download.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+import re
+import requests
+
+word = raw_input("Input key word: ")
+url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word='+word+'&ct=201326592&v=flip'
+result = requests.get(url)
+
+html = requests.get(url).text
+pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
+i = 0
+for each in pic_url:
+    print each
+    try:
+        pic= requests.get(each, timeout=10)
+    except requests.exceptions.ConnectionError:
+        print '【错误】当前图片无法下载'
+        continue
+    string = 'pictures\\'+str(i) + '.jpg'
+    fp = open(string,'wb')
+    fp.write(pic.content)
+    fp.close()
+    i += 1
